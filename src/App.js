94b3733e6799faeb7bed425407c8e14e6291a8f6
EliTimeless <EliTimeless@users.noreply.github.com>
2023-11-06 10:28:05 +0100

    add github repo url

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
       <h1>My weather app</h1>
+      <footer>
+        <p>
+          This project was coded by Eliska Necas and is
+          <a
+            href="https://github.com/EliTimeless/new-react-weather-app.git"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-source code
+          </a>
+        </p>
+      </footer>
     </div>
   );
 }
