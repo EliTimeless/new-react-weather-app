66de205cc880e404e86111018643c96041c8f0d9
EliTimeless <EliTimeless@users.noreply.github.com>
2023-11-07 16:17:19 +0100

    add css

--- src/Weather.js
+++ src/Weather.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Weather.css";
+import "./App.css";
 
 export default function Weather() {
   return (
@@ -11,25 +12,34 @@ export default function Weather() {
               type="search"
               placeholder="Enter a city"
               className="form-control"
+              autoFocus="on"
             />
           </div>
           <div className="col-3">
-            <input type="submit" value="Search" className="btn btn-primary" />
+            <input
+              type="submit"
+              value="Search"
+              className="btn btn-primary w-100"
+            />
           </div>
         </div>
       </form>
       <h1>Prague</h1>
       <ul>
-        <li>Wednesday</li>
+        <li>Wednesday 7:00</li>
         <li>Mostly cloudy</li>
       </ul>
-      <div className="row">
+      <div className="row mt-3">
         <div className="col-6">
-          <img
-            href="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-            alt="partially-cloudy"
-          />
-          <p>6°C</p>
+          <div className="clearfix">
+            <img
+              href="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="mostly-cloudy"
+              className="float-left"
+            />
+            <span className="temperature"> 6</span>
+            <span className="unit">°C</span>
+          </div>
         </div>
         <div className="col-6">
           <ul>
