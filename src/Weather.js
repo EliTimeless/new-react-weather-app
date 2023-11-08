b14fe66363fb0178f270a2c91e64cb731fc883fb
EliTimeless <EliTimeless@users.noreply.github.com>
2023-11-08 14:48:07 +0100

    add axios, useState,Api calls

--- src/Weather.js
+++ src/Weather.js
@@ -1,54 +1,85 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 import "./Weather.css";
 import "./App.css";
 
 export default function Weather() {
-  return (
-    <div className="Weather">
-      <form>
-        <div className="row">
-          <div className="col-9">
-            <input
-              type="search"
-              placeholder="Enter a city"
-              className="form-control"
-              autoFocus="on"
-            />
+  const [temperature, setTemperature] = useState(null); //null protoze zatim nevime hodnotu
+  const [weatherData, setWeatherData] = useState({ ready: false });
+
+  function handleResponse(response) {
+    console.log(response.data);
+    setTemperature(response.data.main.temp);
+
+    setWeatherData({
+      ready: true,
+      temperature: response.data.main.temp,
+      wind: Math.round(response.data.wind.speed),
+      city: response.data.name,
+      humidity: response.data.main.humidity,
+      iconUrl: "https://openweathermap.org/img/wn/10d@2x.png",
+      description: response.data.weather[0].description,
+      date: "Wednesday 7:00",
+    });
+  }
+
+  if (weatherData.ready) {
+    return (
+      <div className="Weather">
+        <form>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+              />
+            </div>
+            <div className="col-3">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
           </div>
-          <div className="col-3">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
+        </form>
+        <h1>{weatherData.city}</h1>
+        <ul>
+          <li>{weatherData.date}</li>
+          <li className="text-capitalize">{weatherData.description}</li>
+        </ul>
+        <div className="row mt-3">
+          <div className="col-6">
+            <div className="clearfix">
+              <img
+                href={weatherData.iconUrl}
+                alt={weatherData.description}
+                className="float-left"
+              />
+              <span className="temperature">{Math.round(temperature)}</span>
+              <span className="unit">
+                {" "}
+                <a href="">°C</a> | <a href="">°F</a>
+              </span>
+            </div>
           </div>
-        </div>
-      </form>
-      <h1>Prague</h1>
-      <ul>
-        <li>Wednesday 7:00</li>
-        <li>Mostly cloudy</li>
-      </ul>
-      <div className="row mt-3">
-        <div className="col-6">
-          <div className="clearfix">
-            <img
-              href="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-              alt="mostly-cloudy"
-              className="float-left"
-            />
-            <span className="temperature"> 6</span>
-            <span className="unit">°C</span>
+          <div className="col-6">
+            <ul>
+              <li>Humidity: {weatherData.humidity}: 72%</li>
+              <li>Wind: {weatherData.wind} km/h</li>
+            </ul>
           </div>
-        </div>
-        <div className="col-6">
-          <ul>
-            <li>Precipitation: 15%</li>
-            <li>Humidity: 72%</li>
-            <li>Wind: 15km/h</li>
-          </ul>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    const apiKey = "ffbbbb548b237aed83af9997c794ee44";
+    let city = "Prague";
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading";
+  }
 }
