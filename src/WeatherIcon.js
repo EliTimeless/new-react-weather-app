20d4a19d9f0c5cd03a382941e8ae398fa90b2794
EliTimeless <EliTimeless@users.noreply.github.com>
2023-11-18 11:53:20 +0100

    update icon color

--- src/WeatherIcon.js
+++ src/WeatherIcon.js
@@ -27,7 +27,7 @@ export default function WeatherIcon(props) {
     return (
       <ReactAnimatedWeather
         icon="CLEAR_DAY"
-        color="#d6d6fe"
+        color="#ffffff"
         size={props.size}
         animate={true}
       />
@@ -36,7 +36,7 @@ export default function WeatherIcon(props) {
     return (
       <ReactAnimatedWeather
         icon={codeMapping[props.code]}
-        color="#d6d6fe"
+        color="#ffffff"
         size={40}
         animate={true}
       />
