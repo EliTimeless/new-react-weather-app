458ac087a363369b849e3bd7bb79c96af98608c8
EliTimeless <EliTimeless@users.noreply.github.com>
2023-11-19 16:14:18 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ export default function App() {
           <p>
             This project was coded by{" "}
             <a
-              href="https://www.linkedin.com/in/eliška-nečasová-front-end-tester"
+              href="https://linkedin.com/in/eliška-nečasová-front-end-tester"
               target="_blank"
               rel="noreferrer"
               title="linkedIn profile"
